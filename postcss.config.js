a183aeb87b74059ff0c3e664b15d726df4f94976
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-05-10 10:54:50 +0100

    Test

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,8 @@
 module.exports = {
-  parser: 'postcss-scss',
   plugins: {
-    'postcss-mixins': {},
-    'autoprefixer': {},
-    'postcss-preset-env': {
-      stage: 1,
-    },
-    'postcss-import': {},
     'postcss-nested': {},
-    'postcss-simple-vars': {},
-    'postcss-for': {}, 
-    
+    tailwindcss: {},
+    autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
