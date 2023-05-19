f72088701c70930fb059f84c9dbf76aeaf05eb4e
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-05-19 15:44:00 +0100

    Initial preview

--- src/blocks/RecentArticles.js
+++ src/blocks/RecentArticles.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Recent from '../components/Article/Recent';
 
-export default function RecentArticles({ data, preview }) {
-  return (
-    <section className='block block__recent-articles'>
-        <h2>{data?.title}</h2>
-        {preview ? 'Articles will show up here' : <Recent />}
-    </section>
-  );
+export default function RecentArticles({ data }) {
+	return (
+		<section className='block block__recent-articles'>
+			<div className="container">
+				<div className="recent-articles__posts post-grid">
+					<Recent/>
+				</div>
+			</div>
+		</section>
+	);
 }
--- src/components/Article/Recent.js
+++ src/components/Article/Recent.js
@@ -6,8 +6,8 @@ export default function Recent() {
   const posts = useRecentArticles();
 
   return (
-    <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6'>
+    <>
       {posts && posts.map((item, i) => <ArticleCard key={i} data={item} />)}
-    </div>
+    </>
   );
 }
