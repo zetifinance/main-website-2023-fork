1e782ca3da04add985324830380c1f4cbe0a51fc
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-14 16:13:36 +0100

    see if this works but im sure ill be back

--- src/blocks/Maps.js
+++ src/blocks/Maps.js
@@ -2,72 +2,75 @@ import clsx from 'clsx';
 import React, { useEffect, useState } from 'react';
 import Text from '../components/UI/Text';
 import Buttons from '../components/UI/Buttons';
-import { useInView } from "react-intersection-observer";
+import { useInView } from 'react-intersection-observer';
+import NoSSR from 'react-no-ssr';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
 export default function Maps({ data }) {
-    // In View
-    const { ref, inView } = useInView({
-        threshold: .3,
-        triggerOnce: true,
+  const { ref, inView } = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
+
+  const [MapContainer, setMapContainer] = useState(null);
+
+  useEffect(() => {
+    import('react-leaflet').then((module) => {
+      setMapContainer(module.MapContainer);
+    });
+  }, []);
+
+  useEffect(() => {
+    const L = require('leaflet');
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+      iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
     });
 
-    // Functions
-    // const [geoAssets, setGeoAssets] = useState([]);
+    // fetch('https://zeti.co.uk/api/geoAssets').then(response => response.json()).then(data => setGeoAssets(data));
+  }, []);
 
-    // useEffect(() => {
-    //     fetch('https://zeti.co.uk/api/geoAssets')
-    //         .then(response => response.json())
-    //         .then(data => setGeoAssets(data));
-    // }, []);
+  // Intro
+  const intro_heading = data?.intro?.heading;
+  const intro_content = data?.intro?.rich_editor;
+  const intro_buttons = data?.intro?.buttons;
 
-    // Intro
-    const intro_heading = data?.intro?.heading;
-    const intro_content = data?.intro?.rich_editor;
-    const intro_buttons = data?.intro?.buttons;
+  // Slide List
+  const maps = data?.maps_grid;
 
-    // Slide List
-    const maps = data?.maps_grid;
+  return (
+    <section inview={inView} className={clsx('block block__maps', { 'in-view': inView })}>
+      <div className="container">
+        <div className="maps__intro">
+          {intro_heading && <h2 className="section-heading">{intro_heading}</h2>}
 
-    return (
-        <section inview={inView} className={clsx('block block__maps', { 'in-view': inView })}>
-            <div className="container">
-                <div className="maps__intro">
-                    {intro_heading && (
-                        <h2 className="section-heading">{intro_heading}</h2>
-                    )}
+          {intro_content && <Text className={clsx('')}>{intro_content}</Text>}
 
-                    {intro_content && (
-                        <Text className={clsx('')}>
-                            {intro_content}
-                        </Text>
-                    )}
+          {intro_buttons && <Buttons buttons={intro_buttons} className={clsx('')} />}
+        </div>
 
-                    {intro_buttons && (
-                        <Buttons buttons={intro_buttons} className={clsx('')} />
-                    )}
-                </div>
-
-                <div className={clsx('maps__grid')}>
-                    <div className="maps__map">
-                        <h3>UK</h3>
-
-                        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "500px", width: "100%" }} scrollWheelZoom={false}>
-                            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                            <Marker position={[51.505, -0.09]}>
-                            </Marker>
-                        </MapContainer>
-                    </div>
-                    <div className="maps__map">
-                        <h3>USA</h3>
-                        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "500px", width: "100%" }} scrollWheelZoom={false}>
-                            <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                            <Marker position={[51.505, -0.09]}>
-                            </Marker>
-                        </MapContainer>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+        <div className={clsx('maps__grid')}>
+          <div className="maps__map">
+            <h3>UK</h3>
+            <NoSSR>
+              {MapContainer && (
+                <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '500px', width: '100%' }} scrollWheelZoom={false}>
+                  <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                  <Marker position={[51.505, -0.09]} />
+                </MapContainer>
+              )}
+            </NoSSR>
+          </div>
+          <div className="maps__map">
+            <h3>USA</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/Head/DefaultHead.js
+++ src/components/Head/DefaultHead.js
@@ -48,8 +48,6 @@ export default function DefaultHead({ data, children }) {
       <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png" />
       <meta name="msapplication-TileImage" content="/img/favicons/ms-icon-144x144.png" />
       <link href="https://api.fontshare.com/v2/css?f[]=general-sans@500,400,600,700&display=swap" rel="stylesheet"></link>
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossOrigin=""/>
-      <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossOrigin=""></script>
     </>
   )
 }
