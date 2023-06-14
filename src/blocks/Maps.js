04f2c87288f8c34b3ae660ce389969aaab594982
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-14 14:12:15 +0100

    Basic leaflet test

--- src/blocks/Maps.js
+++ src/blocks/Maps.js
@@ -53,21 +53,16 @@ export default function Maps({ data }) {
                     <div className="maps__map">
                         <h3>UK</h3>
                         <div>
-                            <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "500px", width: "100%" }}>
+                            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                                 <TileLayer
+                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                                 />
-                                {/* Filter and plot UK markers */}
-                                {geoAssets
-                                    .filter(asset => asset.latitude > 49 && asset.latitude < 61 && asset.longitude > -11 && asset.longitude < 2)
-                                    .map((asset, index) => (
-                                        <Marker key={index} position={[asset.latitude, asset.longitude]}>
-                                            <Popup>
-                                                A pretty CSS3 popup. <br /> Easily customizable.
-                                            </Popup>
-                                        </Marker>
-                                    ))}
+                                <Marker position={[51.505, -0.09]}>
+                                    <Popup>
+                                        A pretty CSS3 popup. <br /> Easily customizable.
+                                    </Popup>
+                                </Marker>
                             </MapContainer>
                         </div>
                     </div>
