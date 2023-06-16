009664be0188f9af79c9c87130c1c078cc00bc7d
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-16 12:09:42 +0100

    Temp footer - this needs to be reworked into CMS :)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,27 +7,27 @@ export default function Footer() {
 				<div className="footer__upper">
 					<div className="footer__column footer__column--address">
 						<h3>London</h3>
+						<p>Zeti Ltd.</p>
 						<p>52 Tabernacle St</p>
 						<p>London</p>
 						<p>EC2A 4NJ</p>
 						<a href="mailto:hello@zeti.group">hello@zeti.group</a>
-						<a href="tel:+44(0) 123 456 7890">+44(0) 123 456 7890</a>
 					</div>
 					<div className="footer__column footer__column--address">
 						<h3>Los Angeles</h3>
+						<p>Zeti, Inc.</p>
 						<p>2450 Colorado Avenue</p>
 						<p>Santa Monica</p>
 						<p>CA 90404</p>
 						<a href="mailto:hellola@zeti.group">hellola@zeti.group</a>
-						<a href="tel:+44(0) 123 456 7890">+44(0) 123 456 7890</a>
 					</div>
 					<div className="footer__column footer__column--address">
 						<h3>New York City</h3>
-						<p>Zeti Inc., c/o NYU Future Lab</p>
+						<p>Zeti, Inc.</p>
+						<p>c/o NYU Future Lab</p>
 						<p>370 Jay Street</p>
 						<p>NY 11201</p>
 						<a href="mailto:hellonyc@zeti.group">hellonyc@zeti.group</a>
-						<a href="tel:+44(0) 123 456 7890">+44(0) 123 456 7890</a>
 					</div>
 					<div className="footer__column footer__column--form">
 						<h3>Stay up to date</h3>
