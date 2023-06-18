351d1bc7349c33334d2e9a88f41f4c10868a7719
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-18 23:49:49 +0100

    Add screen reader to toggle

--- src/components/DarkmodeToggleNew.js
+++ src/components/DarkmodeToggleNew.js
@@ -35,6 +35,7 @@ const LightModeToggle = () => {
           <path strokeLinecap="round" strokeLinejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
         </svg>
       )}
+      <div className="sr-only">Switch color theme</div>
     </button>
   );
 };
