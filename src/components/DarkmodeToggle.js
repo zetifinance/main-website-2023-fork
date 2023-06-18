cec84b0647a5ed2db4ae3657cd2768dc5882cd03
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-18 22:10:03 +0100

    Darkmode initial state test

--- src/components/DarkmodeToggle.js
+++ src/components/DarkmodeToggle.js
@@ -6,7 +6,7 @@ export default function DarkmodeToggle() {
     <ThemeToggler>
       {({ theme, toggleTheme }) => {
         return (
-          <button className="theme-toggle" onClick={(e) => toggleTheme(theme === 'light' ? 'dark' : 'light')} >
+          <button className="theme-toggle" onClick={(e) => toggleTheme(theme === 'dark' ? 'light' : 'dark')} >
             <svg className="h-6 w-6 block dark:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} >
               <path strokeLinecap="round" strokeLinejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
             </svg>
