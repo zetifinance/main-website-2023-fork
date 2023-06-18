f2867b376ae92c8aa84360abe84308e96f1bf31a
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-18 19:33:16 +0100

    Marker image fix?

--- src/blocks/Maps.js
+++ src/blocks/Maps.js
@@ -3,7 +3,17 @@ import React, { useEffect, useState, Suspense } from 'react';
 import Text from '../components/UI/Text';
 import Buttons from '../components/UI/Buttons';
 import { useInView } from 'react-intersection-observer';
+
 import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export default function Maps({ data }) {
   const { ref, inView } = useInView({
