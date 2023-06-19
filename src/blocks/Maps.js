62d955e40af0504ff17bdd90c730e80489d10e4d
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-19 11:57:16 +0100

    Map coords

--- src/blocks/Maps.js
+++ src/blocks/Maps.js
@@ -63,7 +63,7 @@ export default function Maps({ data }) {
             );
 
             setMapModuleUSA(
-              <MapContainer center={[37.6, -120]} zoom={10} style={{ height: '500px', width: '100%' }}>
+              <MapContainer center={[37.6, -122.2]} zoom={10} style={{ height: '500px', width: '100%' }}>
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                   url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
