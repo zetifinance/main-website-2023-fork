bfd4562718e7855d1189238a5fead2d49f0ddfe6
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-19 09:09:09 +0100

    Maps turned back on

--- src/blocks/Maps.js
+++ src/blocks/Maps.js
@@ -4,8 +4,8 @@ import Text from '../components/UI/Text';
 import Buttons from '../components/UI/Buttons';
 import { useInView } from 'react-intersection-observer';
 
-// import 'leaflet/dist/leaflet.css';
-// import iconMarker from 'leaflet/dist/images/marker-icon.png'
+import 'leaflet/dist/leaflet.css';
+import iconMarker from 'leaflet/dist/images/marker-icon.png'
 
 export default function Maps({ data }) {
   const { ref, inView } = useInView({
@@ -19,63 +19,63 @@ export default function Maps({ data }) {
   const intro_buttons = data?.intro?.buttons;
 
   // Maps
-  // const [loadMap, setLoadMap] = useState(false);
-  // const [MapModuleUK, setMapModuleUK] = useState(null);
-  // const [MapModuleUSA, setMapModuleUSA] = useState(null);
-  // const [geoAssets, setGeoAssets] = useState([]);
+  const [loadMap, setLoadMap] = useState(false);
+  const [MapModuleUK, setMapModuleUK] = useState(null);
+  const [MapModuleUSA, setMapModuleUSA] = useState(null);
+  const [geoAssets, setGeoAssets] = useState([]);
   
-  // useEffect(() => {
-  //   fetch('https://zeti.co.uk/api/geoAssets')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setGeoAssets(data);
-  //       setLoadMap(true);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch('https://zeti.co.uk/api/geoAssets')
+      .then(response => response.json())
+      .then(data => {
+        setGeoAssets(data);
+        setLoadMap(true);
+      });
+  }, []);
   
-  // useEffect(() => {
-  //   if (loadMap === true) {
-  //     if (typeof window !== 'undefined') {
-  //       import('leaflet').then((L) => {
-  //         const icon = L.icon({ 
-  //           iconUrl: iconMarker, 
-  //           iconSize: [30, 49],
-  //           iconAnchor: [15, 49]
-  //         });
+  useEffect(() => {
+    if (loadMap === true) {
+      if (typeof window !== 'undefined') {
+        import('leaflet').then((L) => {
+          const icon = L.icon({ 
+            iconUrl: iconMarker, 
+            iconSize: [30, 49],
+            iconAnchor: [15, 49]
+          });
   
-  //         import('react-leaflet').then((leaflet) => {
-  //           const { MapContainer, TileLayer, Marker } = leaflet;
+          import('react-leaflet').then((leaflet) => {
+            const { MapContainer, TileLayer, Marker } = leaflet;
   
-  //           setMapModuleUK(
-  //             <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '500px', width: '100%' }}>
-  //               <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            setMapModuleUK(
+              <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '500px', width: '100%' }}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
     
-  //               {/* Filter and plot UK markers */}
-  //               {geoAssets
-  //                 .filter(asset => asset.latitude > 49 && asset.latitude < 61 && asset.longitude > -11 && asset.longitude < 2)
-  //                 .map((asset, index) => (
-  //                   <Marker key={index} position={[asset.latitude, asset.longitude]} icon={icon} />
-  //                 ))}
-  //             </MapContainer>
-  //           );
+                {/* Filter and plot UK markers */}
+                {geoAssets
+                  .filter(asset => asset.latitude > 49 && asset.latitude < 61 && asset.longitude > -11 && asset.longitude < 2)
+                  .map((asset, index) => (
+                    <Marker key={index} position={[asset.latitude, asset.longitude]} icon={icon} />
+                  ))}
+              </MapContainer>
+            );
 
-  //           setMapModuleUSA(
-  //             <MapContainer center={[37.6, -95.665]} zoom={13} style={{ height: '500px', width: '100%' }}>
-  //               <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            setMapModuleUSA(
+              <MapContainer center={[37.6, -95.665]} zoom={13} style={{ height: '500px', width: '100%' }}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
     
-  //               {/* Filter and plot UK markers */}
-  //               {geoAssets
-  //                 .filter(asset => asset.latitude > 24.396308 && asset.latitude < 49.384358 && asset.longitude > -125.000000 && asset.longitude < -66.934570)
-  //                 .map((asset, index) => (
-  //                   <Marker key={index} position={[asset.latitude, asset.longitude]} icon={icon} />
-  //                 ))}
-  //             </MapContainer>
-  //           );
-  //         });
-  //       });
-  //     }
-  //   }
-  // }, [loadMap]);
+                {/* Filter and plot UK markers */}
+                {geoAssets
+                  .filter(asset => asset.latitude > 24.396308 && asset.latitude < 49.384358 && asset.longitude > -125.000000 && asset.longitude < -66.934570)
+                  .map((asset, index) => (
+                    <Marker key={index} position={[asset.latitude, asset.longitude]} icon={icon} />
+                  ))}
+              </MapContainer>
+            );
+          });
+        });
+      }
+    }
+  }, [loadMap]);
 
   return (
     <section inview={inView} ref={ref} className={clsx('block block__maps', { 'in-view': inView })}>
@@ -91,23 +91,23 @@ export default function Maps({ data }) {
         <div className={clsx('maps__grid')}>
           <div className="maps__map">
             <h3>UK</h3>
-            {/* {typeof window !== 'undefined' && (
+            {typeof window !== 'undefined' && (
               <div className="map">
                 <Suspense fallback={<div>Loading map...</div>}>
                   {MapModuleUK}
                 </Suspense>
               </div>
-            )} */}
+            )}
           </div>
           <div className="maps__map">
             <h3>USA</h3>
-            {/* {typeof window !== 'undefined' && (
+            {typeof window !== 'undefined' && (
               <div className="map">
                 <Suspense fallback={<div>Loading map...</div>}>
                   {MapModuleUSA}
                 </Suspense>
               </div>
-            )} */}
+            )}
           </div>
         </div>
       </div>
