a7706ea7e4ce7d725ed64124c37ab14f65555b7e
Aaron Brewster <aaron@thelonelypixel.co.uk>
2023-06-19 10:35:02 +0100

    Map styles

--- src/blocks/Maps.js
+++ src/blocks/Maps.js
@@ -47,8 +47,11 @@ export default function Maps({ data }) {
             const { MapContainer, TileLayer, Marker } = leaflet;
   
             setMapModuleUK(
-              <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '500px', width: '100%' }}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <MapContainer center={[51.505, -0.09]} zoom={11} style={{ height: '500px', width: '100%' }}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                />
     
                 {/* Filter and plot UK markers */}
                 {geoAssets
@@ -60,8 +63,11 @@ export default function Maps({ data }) {
             );
 
             setMapModuleUSA(
-              <MapContainer center={[37.6, -95.665]} zoom={13} style={{ height: '500px', width: '100%' }}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <MapContainer center={[37.6, -122.4]} zoom={12} style={{ height: '500px', width: '100%' }}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                />
     
                 {/* Filter and plot UK markers */}
                 {geoAssets
